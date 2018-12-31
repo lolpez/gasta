ac92aac6507d57fd61bc7db485a2a1b1a2a770a2
lolpez <luiyicpu@hotmail.com>
2018-12-31 17:42:40 -0400

    Edited sw register

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@
 */
 if ('serviceWorker' in navigator) {
     // Register the service worker
-    navigator.serviceWorker.register('/sw.js').then(reg => {
+    navigator.serviceWorker.register('sw.js').then(reg => {
         reg.addEventListener('updatefound', () => {
             // An updated service worker has appeared in reg.installing!
             newWorker = reg.installing;
