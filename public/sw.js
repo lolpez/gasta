49ec90394958ac8b857dcbc6e7ad1e9b97a6d947
lolpez <luiyicpu@hotmail.com>
2018-12-31 18:55:16 -0400

    Updated app to v10

--- public/sw.js
+++ public/sw.js
@@ -14,7 +14,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'gasta-sw-V9';
+const PRECACHE = 'gasta-sw-V10';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
@@ -27,7 +27,6 @@ self.addEventListener('install', event => {
 	event.waitUntil(
 		caches.open(PRECACHE)
 			.then(cache => cache.addAll(PRECACHE_URLS))
-			.then(self.skipWaiting())
 	);
 });
 
