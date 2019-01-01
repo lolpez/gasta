cc8d1ba170df204488644e3941f3cbe454304062
lolpez <luiyicpu@hotmail.com>
2019-01-01 15:42:01 -0400

    Fixed code for first paint audit

--- public/js/app-ui.js
+++ public/js/app-ui.js
@@ -1,13 +1,18 @@
-(function () {
+window.onload = function () {
+    var loader = document.getElementById('loader');
+    var navbar = document.getElementById('navbar');
+    var content = document.getElementById('content');
     var modals = document.querySelectorAll('.modal');
     var navs = document.querySelectorAll('.sidenav');
     var tabs = document.getElementById('tabs');
     var floatingButton = document.getElementById('floating-button');
     var tooltips = document.querySelectorAll('.tooltipped');
-
     M.Modal.init(modals);
     M.Sidenav.init(navs, {});
     M.Tabs.init(tabs)
     M.FloatingActionButton.init(floatingButton);
     M.Tooltip.init(tooltips);
-})();
+    loader.style.display = 'none';
+    navbar.style.display = 'block';
+    content.style.display = 'block';
+}
